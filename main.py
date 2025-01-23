7aa9345057110282796ae7be6ae2e0f0ef18313c
Sayed Ashfaq <302syedashfaq@gmail.com>
2025-01-23 22:07:12 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import time
+import requests
+from datetime import datetime
+import smtplib
+
+MY_LAT = 51.507351 # Your latitude
+MY_LONG = -0.127758 # Your longitude
+
+my_email = "yourmail@gmail.com"
+password = "securitykey"
+
+def iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    if iss_latitude-5 <= MY_LAT <= iss_latitude+5 and iss_longitude-5 <= MY_LONG <= iss_longitude+5:
+        return True
+
+#Your position is within +5 or -5 degrees of the ISS position.
+
+def is_dark():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0,
+    }
+
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now()
+    if sunset <= time_now.hour <= sunrise:
+        return True
+#If the ISS is close to my current position
+# and it is currently dark
+# Then send me an email to tell me to look up.
+# BONUS: run the code every 60 seconds.
+
+while True:
+    print("Started Running")
+    time.sleep(60)
+    if iss_overhead() and is_dark():
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs="sashfaq229@gmail.com",
+                msg="Subject: LookUp for ISS \n"
+                    "The ISS is currently visible in your area. Step outside, look up, and enjoy the view!"
+            )
+
+
+
+
